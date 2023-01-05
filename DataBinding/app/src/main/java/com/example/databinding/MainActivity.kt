2d7f9179b2d3a96c610fe5641a8e10497e08951f
VinayTShetty <vinaytshetty@gmail.com>
2023-01-05 10:56:43 +0530

    Data Binding Basic Implementation.

--- DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,9 +2,27 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        intiateDataBinding()
+        changeView()
+    }
+
+    private fun intiateDataBinding() {
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+    private fun changeView(){
+        binding.textViewId.text="Vinay T Shetty"
+        binding.btnSetDataId.setOnClickListener(){
+            binding.textViewId.text=binding.editTextId.text
+        }
     }
 }
