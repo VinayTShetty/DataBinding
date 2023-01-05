b45ca27777d530fdaf757dd66cc4d4733408f3b4
VinayTShetty <vinaytshetty@gmail.com>
2023-01-05 18:23:41 +0530

    Assigining Value Using DataBinding Concept.

--- DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,9 +2,29 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * this name is derived from XML after taking _ from the layout file.
+     */
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        /**
+         * Initial setting of the Username and Password
+         */
+        var quotes=Quotes("Vinay","Mandira")
+        binding.quotesDatBnd=quotes
+        /**
+         * after on click assigining new value
+         */
+        binding.btnSetDataId.setOnClickListener(){
+            quotes.userName=binding.editTextId.text.toString()
+            binding.textViewId.text=quotes.userName
+        }
     }
 }
---
+++ DataBinding/app/src/main/java/com/example/databinding/Quotes.kt
@@ -0,0 +1,3 @@
+package com.example.databinding
+
+data class Quotes(var userName:String,var password:String)
